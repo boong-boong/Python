21e0394295e67229768348e93eaae3110a4d8a36
jh868 <2weeks9947@naver.com>
2022-10-26 09:27:40 +0900

    BAEKJOON

---
+++ CodingTest_BAEKJOON/ch.04/10807.py
@@ -0,0 +1,5 @@
+n = int(input())
+li = list(map(int, input().split()))
+f = int(input())
+
+print(li.count(f))
---
+++ CodingTest_BAEKJOON/ch.04/10818.py
@@ -0,0 +1,5 @@
+import sys
+n = int(sys.stdin.readline())
+li = list(map(int, sys.stdin.readline().split()))
+
+print(min(li), max(li))
