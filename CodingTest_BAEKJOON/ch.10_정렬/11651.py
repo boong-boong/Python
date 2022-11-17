90f8b9f1e8577d3ac68921bd4cf7b135338f8bac
jh868 <2weeks9947@naver.com>
2022-11-17 23:30:31 +0900

    BAEKJOON

--- CodingTest_BAEKJOON/ch.10_정렬/11651.py
+++ CodingTest_BAEKJOON/ch.10_정렬/11651.py
@@ -1,10 +1,25 @@
-n = int(input())
+import sys
+n = int(sys.stdin.readline())
 li =[]
 
 for i in range(n):
-    x, y = map(int, input().split())
+    x, y = map(int, sys.stdin.readline().split())
     li.append((y, x))
 
 li.sort()
 for y,x in li:
     print(x,y)
+
+'''
+import sys
+n = int(sys.stdin.readline())
+li =[]
+
+for i in range(n):
+    x ,y = map(int, sys.stdin.readline().split())
+    li.append((x, y))
+
+li.sort(key= lambda x: (x[1], x[0]))
+for x,y in li:
+    print(x,y)
+'''
